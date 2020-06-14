7c553f85fbb8ec5464807d016e44312eb3606e5b
kaneda <dojou1123@gmail.com>
2020-06-14 13:09:59 +0900

    5.2 Staffページ用のレイアウトを生成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  # レイアウトをset
+  layout :set_layout
+
+  private def set_layout
+    # controllerの名前から、staff/admin/customerのどれにマッチするか
+    if params[:controller].match(%r{\A(staff|admin|customer)/})
+      # マッチした1番目を返す
+      Regexp.last_match[1]
+    else
+      # customerを返す
+      "customer"
+    end
+  end
 end

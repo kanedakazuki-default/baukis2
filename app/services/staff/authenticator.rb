01bbbd2afeb93febed1b8e70e245dbb9e484e252
kaneda <dojou1123@gmail.com>
2020-06-27 18:48:59 +0900

    8.2 Staff認証のサービスオブジェクト
    FactoryBotの初期設定
    ファクトリーの定義

---
+++ app/services/staff/authenticator.rb
@@ -0,0 +1,17 @@
+class Staff::Authenticator
+  def initialize(staff_member)
+    @staff_member = staff_member
+  end
+
+  # 与えられたStaffMemberオブジェクトが
+  # nilでない + 有効 + パスワードが設定済みである + 開始日が今日以前 + 終了日が(設定されていない or 今日より後) + パスワードが正しい
+  # 場合にtrueを返す
+  def authenticate(raw_password)
+    @staff_member &&
+        !@staff_member.suspended? &&
+        @staff_member.hashed_password &&
+        @staff_member.start_date <= Date.today &&
+        (@staff_member.end_date.nil? || @staff_member.end_date > Date.today) &&
+        BCrypt::Password.new(@staff_member.hashed_password) == raw_password
+  end
+end

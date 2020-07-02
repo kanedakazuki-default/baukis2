3fdc93f1c278d7b3c1e9c8bbde8d3b6fdc051597
kaneda <dojou1123@gmail.com>
2020-07-03 07:11:53 +0900

    9.2

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root "top#index"
+    get "login" => "sessions#new", as: :login
+    post "session" => "sessions#create", as: :session
+    delete "session" => "sessions#destroy"
+    resources :staff_members
   end
 
   namespace :customer do
